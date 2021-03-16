c27dd56d63198100a110c96e574fb2d406c28491
Solomon Tesfaye <solomon.tesfaye@ocsbstudent.ca>
2021-03-16 17:15:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+ document.getElementById("button").addEventListener("click",myFunction)
+ //this code help to link the html to java script.
+
+ function myFunction() {
+    document.body.style.backgroundColor = "red";}
+   //this code help to display the button done.
